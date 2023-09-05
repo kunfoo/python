06c9b22fad75bb5b56cb434a184158168bd2368d
Kai Katze <kai@kunfoo.org>
2023-09-05 09:46:35 +0200

    yolo

--- syn_push.py
+++ syn_push.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from scapy.all import IP, TCP, sr
 
-ip_packet = IP(dst="10.10.0.74")
+ip_packet = IP(dst="10.1.2.3")
 tcp_packet = TCP(flags="SP", dport=(443))
 print(sr(ip_packet / tcp_packet))
